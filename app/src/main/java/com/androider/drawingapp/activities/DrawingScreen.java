e23821295fcc53be605762dda6271382cbd8f270
Rakesh Rav <rakesh.rao291@gmail.com>
2018-06-01 22:04:32 +0530

    first commit

---
+++ app/src/main/java/com/androider/drawingapp/activities/DrawingScreen.java
@@ -0,0 +1,15 @@
+package com.androider.drawingapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androider.drawingapp.R;
+
+public class DrawingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawing_screen);
+    }
+}
